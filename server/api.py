e9b5094613078bc3c0ce0ba8925f11753f60f5ba
pasdechances <araujo.ludovic@gmail.com>
2024-10-24 00:14:01 +0200

    Update api.py

--- server/api.py
+++ server/api.py
@@ -1,22 +1,29 @@
 import json
 import os.path
+import subprocess
+import threading
 from flask import Flask,  jsonify, request, render_template, send_from_directory
 from flask_cors import CORS
 
-app = Flask(__name__, template_folder='../client')
+app = Flask(__name__, static_folder='../client', template_folder='../client')
 CORS(app)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.abspath(os.path.join(BASE_DIR, '..', 'data'))
 JSON_FILE = os.path.join(DATA_DIR, 'resultats.json')
 
+SCRIPT_RUNNING = False
+
 @app.route('/', methods=['GET'])
 def serve_index():
     return render_template('index.html')
 
-@app.route('/script.js', methods=['GET'])
-def serve_script():
-    return render_template('script.js', name='mark')
+# @app.route('/script.js', methods=['GET'])
+# def serve_script():
+#     return render_template('script.js', name='mark')
+@app.route('/<path:filename>', methods=['GET'])
+def serve_static(filename):
+    return send_from_directory(app.static_folder, filename)
 
 @app.route('/api/duplicates', methods=['GET'])
 def serve_duplicates():
@@ -26,11 +33,61 @@ def serve_duplicates():
 
 @app.route('/api/detection', methods=['POST'])
 def launch_detect():
-    return 0
+    global SCRIPT_RUNNING
+    lock = threading.Lock()
+    if SCRIPT_RUNNING:
+        return jsonify({"status": "error", "message": "Script is already running"}), 409
+    data = request.get_json()
+    script_path = os.path.join(BASE_DIR, 'detect.py')
+    command = ['python', script_path]
+    if isinstance(data.get('image_dir'), str):
+        image_dir = data.get('image_dir')
+        if image_dir:
+            command.extend(['-i', image_dir])
+    if isinstance(data.get('hsize'), int):
+        hsize = data.get('hsize')
+        if hsize:
+            command.extend(['-s', str(hsize)])
+    if isinstance(data.get('tolerance'), int):
+        tolerance = data.get('tolerance')
+        if tolerance:
+            command.extend(['-t', str(tolerance)])
+    try:
+        lock.acquire()
+        SCRIPT_RUNNING = True
+        thread = threading.Thread(target=run_script, args=(command,lock))
+        thread.start()
+        return jsonify({"status": "success", "message": "Detection started started"}), 202
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
 
 @app.route('/api/sort', methods=['POST'])
 def launch_sorter():
-    return 0
+    global SCRIPT_RUNNING
+    lock = threading.Lock()
+    if SCRIPT_RUNNING:
+        return jsonify({"status": "error", "message": "Script is already running"}), 409
+    data = request.get_json()
+    if isinstance(data.get('trash_folder'), int):
+        trash_folder = data.get('trash_folder')
+    script_path = os.path.join(BASE_DIR, 'sorter.py')
+    command = ['python', script_path]
+    if trash_folder:
+        command.extend(['-t', str(trash_folder)])
+    try:
+        lock.acquire()
+        SCRIPT_RUNNING = True
+        thread = threading.Thread(target=run_script, args=(command,lock))
+        thread.start()
+        return jsonify({"status": "success", "message": "Detection started started"}), 202
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+
+@app.route('/api/script-status', methods=['GET'])
+def script_status():
+    global SCRIPT_RUNNING
+    status = "running" if SCRIPT_RUNNING else "idle"
+    return jsonify({"status": status}) 
 
 @app.route('/api/img/keep/<group_id>/<image_id>', methods=['PUT'])
 def keepImg(group_id, image_id):
@@ -62,6 +119,24 @@ def update_result_file(tag, group_id, image_id):
                 img['tag'] = tag
     with open(JSON_FILE, 'w') as f:
         json.dump(results, f, indent=4)
+
+
+
+def run_script(command, script_lock_var):
+    global SCRIPT_RUNNING
+    try:
+        result = subprocess.run(command, capture_output=True, text=True, check=True)
+        print("Script terminé avec succès")
+        print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Erreur lors de l'exécution du script: {str(e)}")
+        print(e.output)
+        print(e.stderr)
+    finally:
+        # Libération du verrou à la fin du script
+        SCRIPT_RUNNING = False
+        script_lock_var.release()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
